91cdc1865e60f7dd5a6ad41c18d2fe67b0d34947
Solaiman  Kmail <psokmail@gmail.com>
2019-07-20 03:05:57 +0300

    change middleware to be group

--- src/CKFinderMiddleware.php
+++ src/CKFinderMiddleware.php
@@ -8,10 +8,7 @@ class CKFinderMiddleware
 {
     public function handle($request, Closure $next)
     {
-        config(['ckfinder.authentication' => function() use ($request) {
-
-            return false;
-        }] );
+        config(['ckfinder.authentication' => auth()->check()] );
 
         return $next($request);
     }
--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,11 @@
 <?php
 
 Route::any('/ckfinder/connector', 'CKSource\CKFinderBridge\Controller\CKFinderController@requestAction')
+    ->middleware('ckfinder')
     ->name('ckfinder_connector');
 
 Route::any('/ckfinder/browser', 'CKSource\CKFinderBridge\Controller\CKFinderController@browserAction')
+    ->middleware('ckfinder')
     ->name('ckfinder_browser');
 
 //Route::any('/ckfinder/examples/{example?}', 'CKSource\CKFinderBridge\Controller\CKFinderController@examplesAction')
